700c20eaddf7fbc1dac01a2c18a70a337a5c89ea
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-11-21 14:34:51 +0000

    [Add]顧客一覧編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,10 +2,13 @@ class Admin::CustomersController < ApplicationController
   def index
     @customer = Customer.all
     @customers = Customer.page(params[:page])
+    @general_customer = GeneralCustomer.all
+    @general_customers = GeneralCustomer.page(params[:id])
   end
 
   def show
     @customer = Customer.find(params[:id])
+    @general_customer = GeneralCustomer.find(params[:id])
   end
 
   def edit
