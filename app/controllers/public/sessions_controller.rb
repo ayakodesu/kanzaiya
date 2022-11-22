9b557cb3392fd0de32e43e01cc89085670456ad3
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-11-22 12:11:34 +0000

    [Add]企業側ログイン

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -20,8 +21,27 @@ class Public::SessionsController < Devise::SessionsController
 
   # protected
 
+
+  def after_sign_in_path_for(resource)
+    public_path
+  end
+
+  def after_sign_out_path_for(resource)
+    public_path
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+   protected
+def customer_state
+  @customer = Customer.find_by(email: params[:customer][:email])
+  return if !@customer
+  if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted == true
+    redirect_to public_path
+  end
+end
+
 end
