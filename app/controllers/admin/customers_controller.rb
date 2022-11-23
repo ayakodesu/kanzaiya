75cb841fad5c87d97a467054f135b82c60c1fc31
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-11-23 14:34:35 +0000

    [Add]顧客側トップページ作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -17,4 +17,20 @@ class Admin::CustomersController < ApplicationController
 
   def edit
   end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:customer_id, :telephone_number, :email, :postal_code, :address, :last_name, :first_name, :last_name_kana, :first_name_kana, :corporation, :is_deleted)
+
+  def general_customer_params
+    params.require(:general_customer).permit(:general_customer_id, :telephone_number, :email, :postal_code, :address, :last_name, :first_name, :last_name_kana, :first_name_kana, :is_deleted)
+
+  end
+  end
+
+
+
+
+
+
 end
