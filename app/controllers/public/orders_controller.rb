4c84e0512549f07e9776000df4185fbd6ade893e
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-11-26 13:23:42 +0000

    [Add]public側注文履歴一覧修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,10 +1,18 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def index
+    @orders = Order.all
   end
 
   def show
   end
+
+private
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
+  end
+
 end
