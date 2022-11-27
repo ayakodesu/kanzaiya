ada07334cc7e15f4b8b7360fd3b51d49b653580a
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-11-27 07:48:33 +0000

    [Add]注文作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,6 +5,7 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+
     new_cart_item = current_customer.cart_items.new(cart_item_params)
     cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
     if cart_item.present?
