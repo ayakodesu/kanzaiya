31414db80e5b35f54fb02f7284d7ad1391edd882
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-11-29 13:03:49 +0000

    [Add]public注文確認金額表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,13 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
-  #def with_tax_price
-    #(price * 1.1).floor
-  #end
+  def betting_rate
+    (last_price * 0.5).floor
+  end
+
+  def with_tax_price
+    (last_price * 1.1).floor
+  end
 
   has_many :cart_items, dependent: :destroy
   has_many :favorites, dependent: :destroy
