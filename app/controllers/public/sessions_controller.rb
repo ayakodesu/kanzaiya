9d42356e91618bb907730402ce73789473b1484c
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-12-06 14:42:28 +0000

    [Add]ゲストログイン機能削除

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -22,11 +22,7 @@ class Public::SessionsController < Devise::SessionsController
   # protected
 
 
-  def guest_sign_in
-    user = User.guest
-    sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
-  end
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -64,10 +64,7 @@ Rails.application.routes.draw do
 
   end
 
-  # ログインゲスト
-  devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
-  end
+
 
 
 
