a265b35c13a35f4cb60a76a3239c96ba3f27c8f3
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-12-09 14:59:58 +0000

    [Add]cart_itemの部分テンプレ作成

--- app/controllers/general_public/cart_items_controller.rb
+++ app/controllers/general_public/cart_items_controller.rb
@@ -1,4 +1,47 @@
 class GeneralPublic::CartItemsController < ApplicationController
+
   def index
+    @cart_items = current_general_customer.cart_items
+    @total = 0
   end
+
+  def create
+    new_cart_item = current_general_customer.cart_items.new(cart_item_params)
+    cart_item = current_general_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    if cart_item.present?
+          cart_item.amount += params[:cart_item][:amount].to_i
+            cart_item.update(amount: cart_item.amount)
+              redirect_to public_cart_items_path
+    elsif new_cart_item.save
+      redirect_to general_public_cart_items_path
+    else
+      redirect_to general_public_cart_items_path
+    end
+  end
+
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to general_public_cart_items_path
+  end
+
+  def destroy_all
+    CartItem.destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to public_cart_items_path
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to general_public_cart_items_path
+  end
+
+
+
+private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
+
 end
--- app/controllers/general_public/orders_controller.rb
+++ app/controllers/general_public/orders_controller.rb
@@ -1,15 +1,68 @@
 class GeneralPublic::OrdersController < ApplicationController
   def new
+    @order = Order.new
+    @addresses = current_general_customer.addresses
+    @customer = current_general_customer
   end
+
+  def confirm
+    @cart_items = current_general_customer.cart_items
+    @order = Order.new(order_params)
+    @order.shipping_cost = 800
+    @select_address = params[:order][:select_address]
+    if @select_address == "0"
+      @order.postal_code = current_general_customer.postal_code
+      @order.address = current_general_customer.address
+      @order.name = current_general_customer.last_name + current_general_customer.first_name
+
+    elsif @select_address == "1"
+      @address_id = params[:order][:address_id]
+      @address = Address.find(@address_id)
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+
+    else
+
+    end
+
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.save
+      current_general_customer.cart_items.each do |cart_item|
+      order_detail = OrderDetail.new
+      order_detail.order_id = @order.id
+      order_detail.item_id = cart_item.item.id
+      order_detail.price = @order.total_payment
+      order_detail.amount = cart_item.amount
+      order_detail.shape = cart_item.shape
+      order_detail.size = cart_item.size
+      order_detail.save
+    end
+    current_general_customer.cart_items.destroy_all
+    redirect_to general_public_orders_complete_path
+
+  end
+
+
+  def complete
+  end
+
 
   def index
     @orders = current_general_customer.orders.page(params[:page])
   end
 
   def show
+   @orders = current_general_customer.orders
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @order.shipping_cost = 800
   end
 
-  private
+private
   def order_params
     params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
   end
