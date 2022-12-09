bab408c66a62ea53bc56efbd1a4d67cc6af75298
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-12-09 14:28:41 +0000

    [Add]itemsのindexページ部分テンプレート作成

--- app/controllers/general_public/items_controller.rb
+++ app/controllers/general_public/items_controller.rb
@@ -1,7 +1,15 @@
 class GeneralPublic::ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @genres = Genre.all.page(params[:page])
+    @items = Item.order('id DESC').limit(8)
   end
 
   def show
   end
+
+  private
+    def item_params
+      params.require(:item).permit(:genre_id, :name, :introduction, :shape, :size, :last_price, :favorite, :is_active, :image, :amount)
+    end
 end
