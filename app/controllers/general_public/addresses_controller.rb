e79cf6a1ce7e21cfcdecddd77c3e496b873280e8
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-12-10 02:07:09 +0000

    [Add]addressのindexページ部分テンプレ作成

--- app/controllers/general_public/addresses_controller.rb
+++ app/controllers/general_public/addresses_controller.rb
@@ -1,7 +1,24 @@
 class GeneralPublic::AddressesController < ApplicationController
+
+  def new
+    @address = Address.new
+  end
+
+
   def index
+    @address = Address.new
+    @addresses = current_general_customer.addresses.page(params[:page])
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
+
+
+  private
+
+  def address_params
+   params.require(:address).permit(:name, :postal_code, :telephone_number, :address, :general_customer_id)
+  end
+
 end
