9e165f44f8f32e8d9e45dbed7715c87b206d88fc
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-12-11 07:38:02 +0000

    [Add]admin注文履歴詳細作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,19 +1,23 @@
 class Admin::OrdersController < ApplicationController
   def show
-    @order = Order.find(params[:id])
+   @order = Order.find(params[:id])
+   @order_details = OrderDetail.page(params[:page])
   end
 
   def index
     @order = Order.new
     @orders = Order.all
-    @customer = Customer.all
-    @general_customer = GeneralCustomer.all
-    @instances = @customer | @general_customer
+    @customer = Order.all
+    @general_customer = Order.all
+    @orders = @customer | @general_customer
+
+
+
 
   end
 
   private
   def order_params
-    params.require(:order).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
+    params.require(:order).permit(:genre_id, :last_name, :first_name, :name, :introduction, :price, :is_active, :image)
   end
 end
