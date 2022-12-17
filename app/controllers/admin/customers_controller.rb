92786e763e2b15d03af96f2ec5a0fd2f59b5791a
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-12-17 08:27:50 +0000

    [Add]admin注文履歴一覧、購入者表示できるように実装

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,16 +1,13 @@
 class Admin::CustomersController < ApplicationController
+
   def index
     @customer = Customer.all
     @general_customer = GeneralCustomer.all
     @instances = @customer | @general_customer
-    
-
-
   end
 
   def show
     @customer = Customer.find(params[:id])
-
   end
 
   def edit
@@ -21,14 +18,12 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
     @customer.update(customer_params)
     redirect_to admin_customer_path(@customer)
-
   end
 
   private
+
   def customer_params
     params.require(:customer).permit(:customer_id, :telephone_number, :email, :postal_code, :address, :last_name, :first_name, :last_name_kana, :first_name_kana, :corporation, :is_deleted)
-
-
   end
 
 
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,5 @@
 class Admin::OrdersController < ApplicationController
+
   def show
    @order = Order.find(params[:id])
    @order_details = OrderDetail.page(params[:page])
@@ -7,25 +8,15 @@ class Admin::OrdersController < ApplicationController
   def index
     @order = Order.new
     #@orders = Order.all
-    @customer = Order.all
-    @general_customer = Order.all
-    @orders = @customer | @general_customer
-
-
-    #puts "test"
-    #@orders.each do |order|
-      #puts "aaa"
-      #puts order.full_name
-    #end
-
-
-
-
-
+    @customer = Customer.all
+    @general_customer = GeneralCustomer.all
+    @instances = @customer | @general_customer
   end
 
   private
+
   def order_params
     params.require(:order).permit(:genre_id, :last_name, :first_name, :name, :introduction, :price, :is_active, :image)
   end
+
 end
