10751da4292cdd726668058cc42b4cf76bae0645
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2023-01-08 10:57:39 +0000

    [Add]購入者と会社名検索実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,13 @@ class Order < ApplicationRecord
   enum status: { waiting_for_payment: 0, payment_confirmation: 1 }
 
   def self.search(search)
-    search ? where('spot LIKE ? OR address LIKE ? OR name LIKE ?', "%#{search}%","%#{search}%","%#{search}%") : all
+    if Rails.env.production?
+    else
+    #customer_ids = Customer.where('first_name LIKE ? OR last_name LIKE ?', "%太郎%", "%太郎%").pluck(:id)
+    #general_customer_ids = GeneralCustomer.where('first_name LIKE ? OR last_name LIKE ?', "%太郎%", "%太郎%").pluck(:id)
+    search ? left_joins(:customer).left_joins(:general_customer).select('orders.id, customers.corporation, orders.customer_id, orders.spot, orders.address, orders.created_at, customers.last_name, customers.first_name, (customers.last_name || customers.first_name) AS c_full_name, general_customers.first_name, general_customers.last_name, (general_customers.last_name || general_customers.first_name) AS g_full_name').
+             where('orders.spot LIKE ? OR orders.address LIKE ? OR customers.corporation LIKE ? OR c_full_name LIKE ? OR g_full_name LIKE ?' ,"%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%") : all
+    end
   end
 
   def full_name
