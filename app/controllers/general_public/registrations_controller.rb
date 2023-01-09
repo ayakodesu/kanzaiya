b6f105c6e7f0ba264fdc0355db24740cb3ffcf7a
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2023-01-09 13:51:03 +0000

    [Add]デザイン修正

--- app/controllers/general_public/registrations_controller.rb
+++ app/controllers/general_public/registrations_controller.rb
@@ -5,7 +5,7 @@ class GeneralPublic::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   def after_sign_up_path_for(resource)
-    general_public_customer_path
+    general_public_path
   end
 
   # GET /resource/sign_up
