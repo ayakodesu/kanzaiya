12d7f8b1b21b0a1e1a7e9ffbebd472bcfb7becb3
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2023-02-05 12:19:10 +0000

    [add]退会ページアイコン追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,6 @@ class Order < ApplicationRecord
   belongs_to :general_customer, optional: true
   has_many :order_details, dependent: :destroy
 
-
   enum payment_method: { credit_card: 0, bank_transfer: 1 }
   enum status: { waiting_for_payment: 0, payment_confirmation: 1 }
 
